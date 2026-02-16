bdbec1d3983997ba81bb2d52a4fcac25a6bffc1d
Nicolás Argañaraz <arganaraznicolas68@gmail.com>
2026-02-16 09:50:31 -0300

    modularizando código main.py

---
+++ consultas_sql.py
@@ -0,0 +1,231 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 16 09:40:42 2026
+
+@author: nicolas
+"""
+
+import numpy as np
+import pandas as pd
+import duckdb as dd
+import matplotlib.pyplot as plt
+from matplotlib import ticker
+import seaborn as sns
+
+# INICIALIZACION DE DATAFRAMES:
+nuestra_carpeta = 'Archivos_Propios/'
+censos = pd.read_csv(nuestra_carpeta + 'censo2010-2022.csv')
+defunciones = pd.read_csv(nuestra_carpeta + 'defunciones.csv')
+clasificacion_de_defunciones = pd.read_csv(nuestra_carpeta + 'clasificacion_de_defunciones.csv')
+departamentos = pd.read_csv(nuestra_carpeta + 'departamentos.csv')
+establecimientos = pd.read_csv(nuestra_carpeta + 'establecimiento.csv')
+provincias = pd.read_csv(nuestra_carpeta + 'provincias.csv')
+
+# %% PUNTO 1: Cobertura de salud
+
+obras_sociales = ('Obra social o prepaga (incluye PAMI)', 
+                 'Programas o planes estatales de salud')
+
+tabla_intermedia = dd.query(
+    f"""
+        SELECT p.nombre AS Provincia,
+        CASE WHEN c.edad < 15 THEN '0 a 14'
+             WHEN c.edad < 35 THEN '15 a 34'
+             WHEN c.edad < 55 THEN '35 a 54'
+             WHEN c.edad < 75 THEN '55 a 74'
+             WHEN c.edad > 74 THEN '75 o más'
+        END AS Rango_etario,
+        c.anio AS Año,
+        c.cantidad AS Cantidad,
+        CASE WHEN c.cobertura_medica IN {obras_sociales} THEN 1
+            ELSE 0 
+        END AS Tiene_cobertura
+        
+        FROM censos AS c
+        INNER JOIN provincias AS p
+            ON c.provincia = p.id                            
+    """).df()
+    
+cobertura_de_salud = dd.query(
+        f"""
+    SELECT 
+    Provincia,
+    Rango_etario,
+    SUM(CASE WHEN (Año = 2010 AND tiene_cobertura = 1) 
+        THEN cantidad 
+        ELSE 0 END)
+        AS Habitantes_con_cobertura_en_2010,
+    SUM(CASE WHEN (Año = 2010 AND tiene_cobertura = 0) 
+        THEN cantidad 
+        ELSE 0 END)
+        AS Habitantes_sin_cobertura_en_2010,  
+    SUM(CASE WHEN (Año = 2022 AND tiene_cobertura = 1)
+        THEN cantidad 
+        ELSE 0 END)
+        AS Habitantes_con_cobertura_en_2022,
+    SUM(CASE WHEN Año = 2022 AND tiene_cobertura = 0 
+        THEN cantidad 
+        ELSE 0 END)
+        AS Habitantes_sin_cobertura_en_2022
+    
+    FROM tabla_intermedia
+    GROUP BY Provincia, Rango_etario
+    ORDER BY Provincia, Rango_etario
+    """).df()
+
+
+# %% PUNTO 2: Establecimientos de salud con terapia intensiva
+
+establecientos_con_terapia_intensiva = dd.query(
+    """
+        SELECT 
+            provincias.nombre AS provincia, 
+            IF(es_publico, 'estatal', 'privado') AS financiamiento,
+            count(*) as cantidad,
+        FROM establecimientos
+        
+        INNER JOIN departamentos 
+            ON departamentos.id = establecimientos.id_departamento
+            
+        INNER JOIN provincias
+            ON provincias.id = departamentos.provincia_id
+            
+        WHERE terapia_intensiva
+        GROUP BY provincias.nombre, establecimientos.es_publico
+        ORDER BY provincias.nombre, financiamiento
+    """).df()
+
+res = dd.query("""
+        SELECT *
+        FROM establecientos_con_terapia_intensiva
+        WHERE provincia = 'Buenos Aires' OR provincia = 'Ciudad Autónoma de Buenos Aires' OR provincia = 'Santa Fe' 
+        ORDER BY provincia 
+
+""").df()
+    
+
+
+# %% PUNTO 3 CAUSAS MUERTE
+jutanda_defunciones = dd.query(
+    """
+        SELECT categoria_defuncion, grupo_edad, sexo, SUM(cantidad) as total
+        FROM defunciones
+        WHERE sexo IN ('masculino', 'femenino')
+        GROUP BY categoria_defuncion, grupo_edad, sexo
+    """    
+).df()
+
+defunciones_mas_frecuentes = dd.query("""
+    SELECT d1.grupo_edad, d1.sexo, d1.categoria_defuncion, d1.total
+    FROM jutanda_defunciones d1
+    WHERE  5 >= ( 
+        SELECT COUNT(*) 
+        FROM jutanda_defunciones d2 
+        WHERE d2.grupo_edad = d1.grupo_edad 
+          AND d2.sexo = d1.sexo 
+          AND d2.total >= d1.total
+    ) 
+    ORDER BY d1.grupo_edad, d1.sexo, d1.total DESC
+""").df()
+
+defunciones_menos_frecuentes = dd.query(
+    """
+        SELECT d1.grupo_edad, d1.sexo, d1.categoria_defuncion, d1.total
+        FROM jutanda_defunciones d1
+        WHERE  5 >= ( 
+            SELECT COUNT(*) 
+            FROM jutanda_defunciones d2 
+            WHERE d2.grupo_edad = d1.grupo_edad 
+              AND d2.sexo = d1.sexo 
+              AND d2.total <= d1.total
+        ) 
+        ORDER BY d1.grupo_edad, d1.sexo, d1.total ASC
+    """    
+).df()
+
+juntada = dd.query(
+    """
+        SELECT *
+        FROM defunciones_mas_frecuentes
+        UNION ALL
+        SELECT *
+        FROM defunciones_menos_frecuentes
+        ORDER BY grupo_edad, sexo, total ASC
+    """    
+).df()
+
+#%% Punto 4: Tasa de mortalidad por provincia
+
+muertes_totales = dd.query(
+    """
+        SELECT p.nombre AS provincia, d.grupo_edad,sum(d.cantidad) AS muertes
+        FROM defunciones d
+        LEFT OUTER JOIN provincias p
+        ON d.provincia_id = p.id
+        WHERE anio = 2022
+        GROUP BY grupo_edad, provincia        
+        ORDER BY grupo_edad,muertes DESC
+        
+    """).df()
+
+#Ahora calculo la cantidad de habitantes por provincia y grupo etario
+habitantes_por_provincia = dd.query(
+    """
+        SELECT p.nombre AS provincia, CASE 
+            WHEN c.edad >= 0 AND c.edad <= 14 THEN '0-14'
+            WHEN c.edad >= 15 AND c.edad <= 34 THEN '15-34'
+            WHEN c.edad >= 35 AND c.edad <= 54 THEN '35-54'
+            WHEN c.edad >= 55 AND c.edad <= 74 THEN '55-74'
+            ELSE '75 o mas'
+            END AS grupo_edad, 
+        sum(c.cantidad) AS habitantes
+        FROM censos c
+        LEFT OUTER JOIN provincias p
+            ON c.provincia = p.id
+        WHERE c.anio = 2022
+        GROUP BY p.nombre,grupo_edad
+        ORDER BY grupo_edad, habitantes DESC
+""").df()
+
+#Finalmente calculo la tasa de mortalidad
+#AVISO: estamos ignorando los datos en los que no sabemos el grupo etario
+tasa_de_mortalidad = dd.query("""
+        SELECT h.provincia, h.grupo_edad, ROUND((m.muertes/h.habitantes)*1000,2) AS tasa
+        FROM habitantes_por_provincia h
+        LEFT OUTER JOIN muertes_totales m                              
+            ON h.grupo_edad = m.grupo_edad AND h.provincia = m.provincia                              
+        ORDER BY h.grupo_edad,tasa
+""").df()
+
+# %% PUNTO 5: Cambios en las causas de defunción
+cantidad_defunciones_2010_2022 = dd.query(
+    """
+        SELECT 
+            clasificacion_de_defunciones.clasificacion_defuncion,
+            SUM(CASE 
+                WHEN anio = 2010 THEN cantidad 
+                ELSE 0 END
+                ) AS def_2010,
+            SUM(
+                CASE WHEN anio = 2022 THEN cantidad 
+                ELSE 0 END
+                ) AS def_2022
+        FROM defunciones
+        INNER JOIN clasificacion_de_defunciones
+            ON defunciones.codigo_defuncion = clasificacion_de_defunciones.codigo_defuncion
+        WHERE anio = 2010 OR anio = 2022
+        GROUP BY clasificacion_de_defunciones.clasificacion_defuncion
+    """).df()
+
+
+diferencia_entre_2010_2022 = dd.query(
+    """
+        SELECT 
+            clasificacion_defuncion,
+            def_2010,
+            def_2022,
+            def_2022 - def_2010 AS diferencia
+        FROM cantidad_defunciones_2010_2022
+        ORDER BY diferencia DESC
+    """).df()
---
+++ visualizacion.py
@@ -0,0 +1,368 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 16 09:38:12 2026
+
+@author: nicolas
+"""
+
+
+import numpy as np
+import pandas as pd
+import duckdb as dd
+import matplotlib.pyplot as plt
+from matplotlib import ticker
+import seaborn as sns
+
+nuestra_carpeta = 'Archivos_Propios/'
+censos = pd.read_csv(nuestra_carpeta + 'censo2010-2022.csv')
+defunciones = pd.read_csv(nuestra_carpeta + 'defunciones.csv')
+clasificacion_de_defunciones = pd.read_csv(nuestra_carpeta + 'clasificacion_de_defunciones.csv')
+departamentos = pd.read_csv(nuestra_carpeta + 'departamentos.csv')
+establecimientos = pd.read_csv(nuestra_carpeta + 'establecimiento.csv')
+provincias = pd.read_csv(nuestra_carpeta + 'provincias.csv')
+#%%
+habitantes_por_provincia = dd.query(
+    """
+        SELECT c.anio, p.nombre, SUM(c.cantidad) AS cantidad_habitantes
+        FROM censos AS c
+        INNER JOIN provincias AS p
+            ON c.provincia = p.id
+        GROUP BY c.anio, p.nombre
+        ORDER BY c.anio, cantidad_habitantes 
+    """).df()
+habitantes_por_provincia = habitantes_por_provincia.pivot(index='nombre', columns='anio', values='cantidad_habitantes')
+
+habitantes_por_provincia = habitantes_por_provincia.sort_values(2022, ascending=False)
+
+fig, ax = plt.subplots()
+
+
+y = np.arange(len(habitantes_por_provincia.index))
+habitantes_2010 = habitantes_por_provincia[2010]
+
+    
+habitantes_2022 = habitantes_por_provincia[2022]
+
+width = 0.4
+
+ax.barh(y - width/2, habitantes_2010, height=width, label='Habitantes 2010')
+ax.barh(y + width/2, habitantes_2022, height=width, label='Habitantes 2022')
+
+ax.set_title('Población Argentina por provincia 2010 vs 2022')
+ax.set_ylabel('Provincias')
+ax.set_xlabel('Cantidad de habitantes (en millones)')
+ax.set_yticks(y, labels=habitantes_por_provincia.index, ha='right')
+ax.legend()
+
+plt.show()
+
+# %% VISUALIZACION PUNTO 2 - DIVISIÓN OPTIMIZADA SEGÚN PERCENTILES
+def percentil_manual(datos, percentil):
+        datos_ordenados = sorted(datos)
+        posicion = (len(datos_ordenados) - 1) * (percentil / 100)
+        i = int(posicion)  # posicion entera
+        
+        return datos_ordenados[i] + (posicion-i) * (datos_ordenados[i + 1] - datos_ordenados[i])
+
+def calcular_percentiles_de_corte(medianas_df):
+    valores = medianas_df['mediana'].tolist()
+    
+    q1 = percentil_manual(valores, 25)
+    q2 = percentil_manual(valores, 50)
+    q3 = percentil_manual(valores, 75)
+    
+    return (
+        int(round(q1)),
+        int(round(q2)),
+        int(round(q3))
+    )
+
+cantidad_defunciones_por_tiempo = dd.query(
+    """
+        SELECT anio, categoria_defuncion, sum(cantidad) as cantidad
+        FROM defunciones
+        GROUP BY anio, categoria_defuncion
+        ORDER BY cantidad DESC
+    """    
+).df()
+
+categorias_df = dd.query(
+    """
+        SELECT DISTINCT categoria_defuncion
+        FROM cantidad_defunciones_por_tiempo
+    """    
+).df()
+
+res = []
+for _, row in categorias_df.iterrows():
+    categoria = row['categoria_defuncion']
+    df = dd.query(
+        f"""
+            SELECT sum(cantidad) as cantidad
+            FROM defunciones
+            WHERE categoria_defuncion = '{categoria}'
+            GROUP BY anio
+        """
+    ).df()
+    mediana = df['cantidad'].median()
+    res.append({'categoria': categoria, 'mediana': mediana})
+
+medianas_df = pd.DataFrame(res)
+
+(corte_grupo1,corte_grupo2,corte_grupo3) = calcular_percentiles_de_corte(medianas_df)
+
+grupo1 = medianas_df[medianas_df['mediana'] <= corte_grupo1]['categoria'].tolist()  
+grupo2 = medianas_df[(medianas_df['mediana'] > corte_grupo1) & (medianas_df['mediana'] <= corte_grupo2)]['categoria'].tolist()
+grupo3 = medianas_df[(medianas_df['mediana'] > corte_grupo2) & (medianas_df['mediana'] <= corte_grupo3)]['categoria'].tolist()
+grupo4 = medianas_df[medianas_df['mediana'] > corte_grupo3]['categoria'].tolist()
+
+
+def graficar_grupo(grupo, titulo):
+    
+    datos_grupo = cantidad_defunciones_por_tiempo[cantidad_defunciones_por_tiempo['categoria_defuncion'].isin(grupo)]
+    
+    fig, ax = plt.subplots(figsize=(20, 8))
+    
+    sns.lineplot(data=datos_grupo, 
+                 x='anio', y='cantidad', 
+                 hue='categoria_defuncion', 
+                 marker='o')
+    años_unicos = sorted(datos_grupo['anio'])
+    
+    ax.set_xticks(años_unicos)
+    ax.set_xticklabels(años_unicos)
+    
+    ax.set_xlabel('Año', fontsize=12)
+    ax.set_ylabel('Cantidad de defunciones', fontsize=12)
+    
+    ax.set_title(f'{titulo}', fontsize=14, fontweight='bold')
+    
+    ax.legend(title='Categoría', loc='upper left')
+    plt.tight_layout()
+    plt.show()
+
+
+graficar_grupo(grupo1, f"GRUPO 1 (0 - {corte_grupo1})")
+graficar_grupo(grupo2, f"GRUPO 2 ({corte_grupo1} - {corte_grupo2})")
+graficar_grupo(grupo3, f"GRUPO 3 ({corte_grupo2} - {corte_grupo3})")
+graficar_grupo(grupo4, f"GRUPO 4 ({corte_grupo3} - ...)")
+
+
+# %% VISUALIZACION PUNTO 3 - Tasa de mortalidad por provincia
+
+muertes_totales_vis = dd.query(
+    """
+        SELECT p.nombre AS provincia, sum(d.cantidad) AS muertes
+        FROM defunciones d
+        LEFT OUTER JOIN provincias p
+        ON d.provincia_id = p.id
+        WHERE anio = 2022
+        GROUP BY provincia        
+        ORDER BY muertes DESC
+        
+    """).df()
+
+#calculo la cantidad de habitantes por provincia
+habitantes_por_provincia_vis = dd.query(
+    """
+        SELECT p.nombre AS provincia, sum(c.cantidad) AS habitantes
+        FROM censos c
+        LEFT OUTER JOIN provincias p
+            ON c.provincia = p.id
+        WHERE c.anio = 2022
+        GROUP BY p.nombre
+        ORDER BY habitantes DESC
+""").df()
+
+#calculo la tasa de mortalidad
+tasa_de_mortalidad_vis = dd.query("""
+        SELECT h.provincia, ROUND((m.muertes/h.habitantes)*1000,2) AS tasa
+        FROM habitantes_por_provincia_vis h
+        LEFT OUTER JOIN muertes_totales_vis m                              
+            ON h.provincia = m.provincia                              
+        ORDER BY tasa DESC
+""").df()
+
+#GRAFICO 1
+fig, ax = plt.subplots()
+ax.barh(data=tasa_de_mortalidad_vis, y='provincia', width='tasa')
+
+ax.set_title('Tasa de mortalidad por provincia')
+ax.set_xlabel('Tasa (cada 1000 habitantes)', fontsize='medium')                       
+ax.set_ylabel('Provincia', fontsize='medium')
+
+#achico los nombres de las provincias
+ax.tick_params(axis = 'y',labelsize = 8)
+
+#--------parte 2
+
+muertes_totales_vis2 = dd.query(
+    """
+        SELECT p.nombre AS provincia, sum(d.cantidad) AS muertes, sexo
+        FROM defunciones d
+        LEFT OUTER JOIN provincias p
+        ON d.provincia_id = p.id
+        WHERE anio = 2022
+        GROUP BY provincia, sexo        
+        ORDER BY muertes DESC
+        
+    """).df()
+
+#calculo la cantidad de habitantes por provincia
+habitantes_por_provincia_vis2 = dd.query(
+    """
+        SELECT p.nombre AS provincia, sum(c.cantidad) AS habitantes, 
+        CASE
+            WHEN c.sexo = 'Varón' THEN 'masculino'
+            WHEN c.sexo = 'Mujer' THEN 'femenino'
+            ELSE 'desconocido'
+        END AS sexo
+        FROM censos c
+        LEFT OUTER JOIN provincias p
+            ON c.provincia = p.id
+        WHERE c.anio = 2022
+        GROUP BY p.nombre, sexo
+        ORDER BY habitantes DESC
+""").df()
+
+#calculo la tasa de mortalidad
+tasa_de_mortalidad_vis2 = dd.query("""
+        SELECT h.provincia, ROUND((m.muertes/h.habitantes)*1000,2) AS tasa, m.sexo
+        FROM habitantes_por_provincia_vis2 h
+        LEFT OUTER JOIN muertes_totales_vis2 m                              
+            ON h.provincia = m.provincia AND h.sexo = m.sexo                              
+        ORDER BY tasa ASC
+""").df()
+
+
+#GRAFICO 2
+fig, ax = plt.subplots(figsize = (8,6))
+sns.barplot(data = tasa_de_mortalidad_vis2, y='provincia', x = 'tasa',hue = 'sexo',orient='h',ax=ax,
+            palette={'femenino':'#e851cc', 'masculino':'#026cb8'})
+
+
+ax.set_title('Tasa de mortalidad por provincia')
+ax.set_xlabel('Tasa (cada 1000 habitantes)', fontsize='medium')                       
+ax.set_ylabel('Provincia', fontsize='medium')
+
+#achico los nombres de las provincias
+ax.tick_params(axis = 'y',labelsize = 8)
+ax.legend(title = 'sexo')
+
+
+
+# %%
+
+establecimientos_por_depto = dd.query("""
+    SELECT id_departamento, COUNT(*) as cantidad
+    FROM establecimientos
+    GROUP BY id_departamento
+    ORDER BY id_departamento
+""").df()
+
+establecimientos_por_departamento = dd.query("""
+    SELECT  e.id_departamento, d.nombre,p.nombre as provincia,e.cantidad
+    FROM establecimientos_por_depto AS e
+    INNER JOIN departamentos as d
+    ON e.id_departamento = d.id
+    INNER JOIN provincias AS p
+    ON d.provincia_id = p.id
+    ORDER BY d.provincia_id, e.cantidad DESC
+""").df()
+
+def calcular_percentiles_de_corte(df, columna='mediana'):
+    """
+    Calcula los percentiles 25, 50 y 75 para crear 4 grupos
+    """
+    p25 = df[columna].quantile(0.25)
+    p50 = df[columna].quantile(0.50)
+    p75 = df[columna].quantile(0.75)
+    return p25, p50, p75
+
+res = []
+provincias_unicas = establecimientos_por_departamento['provincia'].unique()
+
+for provincia in provincias_unicas:
+    datos_provincia = establecimientos_por_departamento[
+        establecimientos_por_departamento['provincia'] == provincia
+    ]
+    
+    mediana = datos_provincia['cantidad'].median()
+    res.append({'provincia': provincia, 'mediana': mediana})
+
+medianas_provincias_df = pd.DataFrame(res)
+(corte_grupo1, corte_grupo2, corte_grupo3) = calcular_percentiles_de_corte(medianas_provincias_df)
+
+grupo1 = medianas_provincias_df[medianas_provincias_df['mediana'] <= corte_grupo1]['provincia'].tolist()
+grupo2 = medianas_provincias_df[(medianas_provincias_df['mediana'] > corte_grupo1) & 
+                                (medianas_provincias_df['mediana'] <= corte_grupo2)]['provincia'].tolist()
+grupo3 = medianas_provincias_df[(medianas_provincias_df['mediana'] > corte_grupo2) & 
+                                (medianas_provincias_df['mediana'] <= corte_grupo3)]['provincia'].tolist()
+grupo4 = medianas_provincias_df[medianas_provincias_df['mediana'] > corte_grupo3]['provincia'].tolist()
+
+for i, grupo in enumerate([grupo1,grupo2,grupo3,grupo4], 1):
+    # Filtrar datos para este grupo
+    datos_grupo = establecimientos_por_departamento[
+        establecimientos_por_departamento['provincia'].isin(grupo)
+    ]
+    
+    # Crear figura
+    plt.figure(figsize=(14, 8))
+    
+    # Crear boxplot
+    sns.boxplot(data=datos_grupo, 
+                x='provincia', 
+                y='cantidad')
+    
+    # Personalizar
+    plt.title(f'Grupo {i}: Distribución de establecimientos de salud por departamento', 
+              fontsize=14)
+    plt.xlabel('Provincia')
+    plt.ylabel('Cantidad de establecimientos por departamento')
+    plt.xticks(rotation=45, ha='right')
+    plt.tight_layout()
+    plt.show()
+    
+# %% VISUALIZACION PUNTO 6
+
+establecimientos_por_provincia = dd.query(
+    """
+        SELECT p.nombre AS provincia, COUNT(*) AS cant_establecimientos
+        FROM df_establecimientos e
+        INNER JOIN df_departamentos d ON e.id_departamento = d.id
+        INNER JOIN df_provincias p ON d.provincia_id = p.id
+        GROUP BY p.nombre
+        ORDER BY cant_establecimientos DESC
+    """).df()
+    
+tasas_por_provincia = dd.query(
+    """
+        SELECT m.provincia, m.tasa AS tasa_mortalidad,
+            ROUND((e.cant_establecimientos/h.habitantes)*10000,2) AS tasa_establecimientos
+        FROM tasa_de_mortalidad_vis m
+        INNER JOIN establecimientos_por_provincia e
+            ON m.provincia = e.provincia
+        INNER JOIN habitantes_por_provincia_vis h
+            ON m.provincia = h.provincia
+    """).df()
+
+fig, ax = plt.subplots()
+
+ax.scatter(data=tasas_por_provincia,
+           x='tasa_establecimientos',
+           y='tasa_mortalidad',
+           s=12,
+           color='red')
+
+for i, row in tasas_por_provincia.iterrows():
+    ax.text(row['tasa_establecimientos'],  
+            row['tasa_mortalidad'],        
+            row['provincia'],              
+            fontsize=9)
+
+ax.set_title('Tasas por provincias')
+ax.set_xlabel('Tasa de cantidad de establecimientos')
+ax.set_ylabel('Tasa de mortalidad')
+ 
+plt.show()
