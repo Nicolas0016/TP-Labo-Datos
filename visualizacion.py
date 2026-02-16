57fd9acb7bcdf2ddf94d7c615ac9e4ed645a3222
Nicolás Argañaraz <arganaraznicolas68@gmail.com>
2026-02-16 12:09:16 -0300

    Grupo axel

--- visualizacion.py
+++ visualizacion.py
@@ -281,8 +281,10 @@ grupo1 = ['Jujuy', 'Misiones', 'Chubut', 'La Pampa', 'Río negro']
 grupo2 = ['CABA','Formosa','La Rioja', 'Catamarca','Santa Cruz']
 grupo3 = ['Corrientes', 'Entre Ríos', 'San Luis', 'Chaco', 'Santiago del Estero']
 grupo4 = ['San Juan', 'Salta', 'Mendoza', 'Tierra del Fuego']
+
 grupo5 = ['Buenos Aires', 'Tucumán', 'Neuquén','Córdoba', 'Santa Fe']
 
+grupo_axel = ['CABA', 'Buenos Aires','La Pampa', 'Córdoba','Chubut','Neuquén','Santa Fe','Santa Cruz']
 for i, grupo in enumerate([grupo1,grupo2,grupo3,grupo4, grupo5], 1):
     # Filtrar datos para este grupo
     datos_grupo = establecimientos_por_departamento[
@@ -321,7 +323,26 @@ plt.ylabel('Cantidad de establecimientos por departamento')
 plt.xticks(rotation=45, ha='right')
 plt.tight_layout()
 plt.show()
-    
+
+datos_grupo = establecimientos_por_departamento[
+    establecimientos_por_departamento['provincia'].isin(grupo_axel)
+]
+
+plt.figure(figsize=(11, 9))
+
+
+sns.boxplot(data=datos_grupo, 
+            x='provincia', 
+            y='cantidad',
+            gap=0)
+
+plt.title(f'Grupo {i}: Distribución de establecimientos de salud por departamento', 
+          fontsize=14)
+plt.xlabel('Provincia')
+plt.ylabel('Cantidad de establecimientos por departamento')
+plt.xticks(rotation=45, ha='right')
+plt.tight_layout()
+plt.show()
 # %% VISUALIZACION PUNTO 6
 
 establecimientos_por_provincia = dd.query(
